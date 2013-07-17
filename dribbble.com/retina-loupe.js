4f319b2efcec566afc913f833baa5d44a8c6271f
Mike Meyer <mikemeyer@gmail.com>
2013-07-16 20:41:57 -0700

    Special tags and a retina loupe fix

--- dribbble.com/retina-loupe.js
+++ dribbble.com/retina-loupe.js
@@ -5,8 +5,7 @@ $(function(){
 
 	console.log('Retina shot URL: '+retinaShotURL);
 
-	if( retinaShotURL === undefined )
-		return false;
+	if( retinaShotURL === undefined ) {return false;}
 
 	// We have a retina shot!
 	$shotContainer.removeClass('retina-unavailable');
@@ -20,53 +19,63 @@ $(function(){
 		var bkgOffsetY = img.height/2 < 300 ? Math.ceil((300-img.height/2)/2) : 0;
 
 		var loupeVisible = false;
-		// TODO: Conditional pixel perfect canvas magic for the loupe as well.
-
-		$retinaLoupe = $('<div id="retina-loupe"><div style="background-image:url('+retinaShotURL+');"></div></div>').appendTo($shotContainer);
+		var $retinaLoupe = $('<div id="retina-loupe"><div style="background-image:url('+retinaShotURL+');"></div></div>').appendTo($shotContainer);
 
 		// Hide loupe if page is inactive
-		function handleVisibilityChange() {
+		var handleVisibilityChange = function() {
 			if (document.webkitHidden) {
 				console.log('hide loupe!!');
 				$shotContainer.addClass('loupe-hidden');
 			} else {
 				console.log('show loupe maybe!!');
 			}
-		}
+		};
 
 		document.addEventListener("webkitvisibilitychange", handleVisibilityChange, false);
+
+		var updateLoupeLocation = function(e){
+			var os = $shotContainer.offset();
+			var mouseX = e.pageX-os.left;
+			var mouseY = e.pageY-os.top;
+			if( mouseX >= 0 && mouseX <= 440 && mouseY >= 0 && mouseY <= 340 ){
+				if (!loupeVisible){
+					$shotContainer.removeClass('loupe-hidden');
+				}
+				loupeVisible = true;
+
+				// We're in bidness
+				$retinaLoupe.css({
+					left: mouseX,
+					top: mouseY
+				}).children('div').css({
+					backgroundPosition: (75-mouseX*2+40+bkgOffsetX)+'px '+(75-mouseY*2+40+bkgOffsetY)+'px'
+				});
+			} else {
+				if (loupeVisible){
+					$shotContainer.addClass('loupe-hidden');
+				}
+				loupeVisible = false;
+			}
+		};
+		/*
+		var oddeven = 0;
+		$('.twotimes').click(function(){
+			if(oddeven % 2){
+				$(document).unbind('.retinaloupe');
+				console.log('LOUPE DISABLED');
+			} else {
+				$(document).bind('mousemove.retinaloupe',updateLoupeLocation);
+				console.log('LOUPE ENABLED');
+			}
+			oddeven++;
+		});
+		*/
 
 		// TODO: check #main for full-800 class (expanded retina shot)
 
 		$shotContainer.addClass('loupe-hidden').hover(
 			function(){
-				$(document).bind('mousemove.retinaloupe',function(e){
-					var os = $shotContainer.offset();
-					var mouseX = e.pageX-os.left;
-					var mouseY = e.pageY-os.top;
-					if(
-						mouseX >= 0 &&
-						mouseX <= 440 &&
-						mouseY >= 0 &&
-						mouseY <= 340
-					){
-						if (!loupeVisible)
-							$shotContainer.removeClass('loupe-hidden');
-						loupeVisible = true;
-
-						// We're in bidness
-						$retinaLoupe.css({
-							left: mouseX,
-							top: mouseY
-						}).children('div').css({
-							backgroundPosition: (75-mouseX*2+40+bkgOffsetX)+'px '+(75-mouseY*2+40+bkgOffsetY)+'px'
-						});
-					} else {
-						if (loupeVisible)
-							$shotContainer.addClass('loupe-hidden');
-						loupeVisible = false;
-					}
-				})
+				$(document).bind('mousemove.retinaloupe',updateLoupeLocation)
 			},
 			function(){
 				// $(document).unbind('mousemove.retinaloupe');
---
+++ dribbble.com/special-tags.coffee
@@ -0,0 +1,70 @@
+ignored_caps = ['and','of', 'the']
+
+replacements =
+	tool:
+		'^cs$':  'CS'
+
+	# Foundries
+	typeface:
+		'^df$':  'DF'
+		'^din$': 'DIN'
+		'^ff$':  'FF'
+		'^fr$':  'FR'
+		'^gt$':  'GT'
+		'^hfj$': 'H&FJ'
+		'^htf$': 'HTF'
+		'^itc$': 'ITC'
+		'^let$': 'LET'
+		'^lt$':  'LT'
+		'^mr$':  'Mr.'
+		'^mrs$': 'Mrs.'
+		'^ms$':  'MS'
+		'^mt$':  'MT'
+		'^pt$':  'PT'
+		'^se$':  'SE'
+		'^tj$':  'TJ'
+		'^vag$': 'VAG' # LOL
+		'^y2k$': 'Y2K'
+
+$tags = $('#tags')
+
+if $tags.length
+
+	$newTagContainer = $('<ol class="tags group">').prependTo($tags)
+
+	formatSlug = (slug, category) ->
+		list = slug.split('-')
+		formatted = []
+
+		slug = slug.toLowerCase()
+
+		for word, i in list
+			if word not in ignored_caps or (i is 0 and word in ignored_caps)
+				word = word.charAt(0).toUpperCase()+word.substr(1)
+
+			if category in replacements
+				for regex, substitute of replacements[category]
+					# TODO: Break on match
+					word = word.replace("/#{regex}/gi", substitute)
+
+			formatted.push word
+
+		return formatted.join(' ')
+
+	formatTags = (category) ->
+		$("a[href*='tags/#{category}:'][rel='tag']", $tags).each ->
+			$tag = $(@)
+			$s = $('strong', $tag);
+
+			slug = $s.text().replace "#{category}:",''
+			text = formatSlug(slug, category)
+
+			$li = $tag.parent()
+			$li.appendTo $newTagContainer
+
+			$li.addClass "tag-#{category} special-tag"
+
+			$s.html "<span class='key'>#{category}</span><span class='val'>#{text}</span>"
+
+	formatTags 'tool'
+	formatTags 'typeface'
