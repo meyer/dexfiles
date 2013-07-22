41f345eda8ed41d4c32e2f910da3c47b40266a93
Mike Meyer <mikemeyer@gmail.com>
2013-07-21 22:26:32 -0700

    Retina shot indicator for THE GRID

--- dribbble.com/big-shots.js
+++ dribbble.com/big-shots.js
@@ -3,6 +3,10 @@ $(function(){
 		var $div = $(this);
 		var retinaURL = $('div[data-media]', $div).data('src').replace('_still.gif', '.gif');
 
+		if ( ~retinaURL.indexOf("_1x.") ) {
+			$div.closest('li').addClass('retina-shot');
+		}
+
 		// This seems excessive.
 		$div.after('<img src="' + retinaURL + '">').remove();
 	});
